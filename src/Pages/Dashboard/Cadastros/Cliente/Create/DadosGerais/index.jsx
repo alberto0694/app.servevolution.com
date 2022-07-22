936055db0ce2c514fe6f1adc776dd53aab71529e
unknown <alberto.pimentel.94@gmail.com>
2022-07-22 17:16:48 -0300

    ajustes 2

--- src/Pages/Dashboard/Cadastros/Cliente/Create/DadosGerais/index.jsx
+++ src/Pages/Dashboard/Cadastros/Cliente/Create/DadosGerais/index.jsx
@@ -48,9 +48,9 @@ export default function ClienteCreate({cliente, setCliente}) {
                             <input type="file" id="imageUpload" accept=".png, .jpg, .jpeg" onChange={(e) => onFileChange(e)} />
                             <label htmlFor="imageUpload"></label>
                         </div>
-                        
+
                         <div className="avatar-preview">
-                            <div id="imagePreview" style={{ backgroundImage: cliente.pessoa?.foto ? `url(${cliente.pessoa?.foto})` : "url('images/contacts/user.jpg')" }}>
+                            {/* <div id="imagePreview" style={{ backgroundImage: cliente.pessoa?.foto ? `url(${cliente.pessoa?.foto})` : "url('images/contacts/user.jpg')" }}> */}
                             </div>
                         </div>
                     </div>
